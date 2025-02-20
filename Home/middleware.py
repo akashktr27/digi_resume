c635baf0f02300af4b6bfa1e7dc3762cbff2dd36
Akash Kantrikar <82324093+akashktr27@users.noreply.github.com>
2025-02-20 15:45:17 +0530

    add ip tracker for their country (#2)

--- Home/middleware.py
+++ Home/middleware.py
@@ -1,6 +1,8 @@
 from django.utils.deprecation import MiddlewareMixin
 from Home.models import *
 from django.urls import resolve
+from celery import shared_task
+import requests
 
 class ClientMetadataMiddleware(MiddlewareMixin):
     def process_request(self, request):
@@ -29,19 +31,34 @@ class ClientMetadataMiddleware(MiddlewareMixin):
                     post_data=str(post_data)  # Save POST data as a string
                 )
             elif view_name == 'download':  # Replace with your specific view name
+                region_name = "NULL"
+                try:
+                    ip_address = "5.151.123.125"
+                    response = requests.get(
+                        f"http://api.ipstack.com/{ip_address}?access_key=a501e6be75e9deb3e600c1eba5fc59d2")
+                    data = response.json()
+                    region_name = data["region_name"]
+                except Exception as e:
+                    region_name = "NULL"
+                    print(f"Error fetching IP details: {e}")
+
                 ResumeViewMetadata.objects.create(
                     ip_address=ip_address,
                     user_agent=user_agent,
                     referrer=referrer,
-                    view_name=view_name
+                    view_name=view_name,
+                    resume_region_name=region_name
                 )
             elif view_name == 'index':
                 # Save general request metadata to the ClientMetadata model
+
+
                 ClientMetadata.objects.create(
                     ip_address=ip_address,
                     user_agent=user_agent,
-                    referrer=referrer
+                    referrer=referrer,
                 )
+                # fetch_and_save_ip_details.delay(metadata.id, ip_address)
 
     def get_client_ip(self, request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -51,3 +68,15 @@ class ClientMetadataMiddleware(MiddlewareMixin):
             ip = request.META.get('REMOTE_ADDR')
         return ip
 
+
+
+@shared_task
+def fetch_and_save_ip_details(metadata_id, ip_address):
+    try:
+        ip_address = "5.151.123.125"
+        response = requests.get(f"http://api.ipstack.com/{ip_address}?access_key=a501e6be75e9deb3e600c1eba5fc59d2")
+        data = response.json()
+        print(data.region_name)
+        ClientMetadata.objects.filter(id=metadata_id).update(region_name=data.region_name)
+    except Exception as e:
+        print(f"Error fetching IP details: {e}")
